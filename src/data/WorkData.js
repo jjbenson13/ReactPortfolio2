9cfbf6f70856002f57462e0de57aaf16d4baea28
Jasmine Benson <jjortega2010@gmail.com>
2021-11-23 21:14:51 -0800

    updated work page with project I've worked on

---
+++ src/data/WorkData.js
@@ -0,0 +1,33 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: "EN-Route",
+    description: "Ever gone travelling and didn’t know if you had enough time to watch your favorite shows and movies? Now, you can automatically sort through and create a list of shows and movies based on time and perfectly calculate a watch plan. Never run out of time to watch your favorite movies and shows again with EN-route. The 'EN' is for Entertainment.",
+    tags: ["HTML", "CSS", "Javascript", "APIs: MapQuest, TVmaze and OMDB", "Local storage"],
+
+    demo: "https://ssterling27.github.io/EN-Route/",
+    github: "https://github.com/ssterling27/EN-Route"
+  },
+  {
+    id: 2,
+    name: "Moodify",
+    description: "Moodify is an application that allows users to search for their favorite songs and create playlists based on their mood. When you search for songs, you will receive the artist name, artwork cover, track title, and the stream link if you wish to watch their music video. This app can create customizable playlists according to your tastes without those unwanted songs.",
+    tags: ["Foundation for CSS", "Deployed with Heroku", "SoundCloud API", "Node.js", "Express.js", "mysql2", "passport", "passport- jwt", "passport - local - sequelize", "sequelize"],
+
+    demo: "https://moodify-27.herokuapp.com",
+    github: "https://github.com/ssterling27/Project2"
+
+  },
+  {
+    id: 3,
+    name: "CryptoMaster",
+    description: "CryptoMaster is a play investment game with virtual $1000 which is given when users register for the website. Play the game for a week and be able to see their weekly rankings by their profit. The market in the game will be the same as the real-time market(Stock or Cryptocurrency).",
+    tags: ["Axios", "Dotenv", "Passport", "Json web token", "React Bootstrap", "Mongoose / MongoDB", "React Router", "Node Js", "Express.If - env", "Concurrently", "Passport", "FontAwesome", "CoinGecko API"],
+
+    demo: "https://vast-headland-48199.herokuapp.com/signin",
+    github: "https://github.com/Lan714/CryptoMaster"
+  },
+  
+]
